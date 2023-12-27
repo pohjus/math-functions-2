3ffcf9064fc642e83d80dbec8e1427a39eda2fa4
pohjus <jussi.pohjolainen@gmail.com>
2023-12-27 16:55:22 +0200

    feat: simple index.js change

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ const { add, subtract, multiply, divide } = require("./mathFunctions");
 console.log("Addition: " + add(5, 5));
 console.log("Subtraction: " + subtract(10, 3));
 console.log("Multiplication: " + multiply(5, 7));
-console.log("Division: " + divide(20, 3));
+console.log("Division: " + divide(20, 1));
