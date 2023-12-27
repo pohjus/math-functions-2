86f8e27c374b82342e5a4ea8db772726470ff7ac
pohjus <jussi.pohjolainen@gmail.com>
2023-12-27 19:22:42 +0200

    feat: mistake in mathFunctions.js

--- src/mathFunctions.js
+++ src/mathFunctions.js
@@ -1,7 +1,7 @@
 module.exports = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
-  multiply: (a, b) => a * b,
+  multiply: (a, b) => a - b,
   divide: (a, b) => {
     if (b === 0) {
       throw new Error("Division by zero is not allowed.");
